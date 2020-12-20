7c7a3d800c88fdae7f3ab43528409ad43c483b86
Tanjim <th3pizzaman15@gmail.com>
2020-12-20 13:17:54 -0500

    added gradable item, finished course integration with firebase

--- app/src/main/java/utils/CourseUtils.java
+++ app/src/main/java/utils/CourseUtils.java
@@ -12,7 +12,11 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+
 import models.Course;
+
+import static android.content.ContentValues.TAG;
 
 public class CourseUtils {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -21,7 +25,7 @@ public class CourseUtils {
         db.collection("courses").document(course.getName()).set(course).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Log.d("SUCCESS", "Course was successfully added with ID");
+                Log.d("SUCCESS", "Course was successfully added");
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -35,7 +39,35 @@ public class CourseUtils {
        return db.collection("courses").document(name);
     }
 
+    /**
+     * Gets all courses from the database
+     * @return Task<QuerySnapshot> object return. Requires implementation onSuccessListener
+     */
     public Task<QuerySnapshot> getAllCourses(){
         return db.collection("courses").get();
+    }
+
+    /**
+     * Adds a gradable item to course
+     * @param course The course where the gradable item will be added
+     * @param gradableItemName name of the gradable item
+     * @param dueDate due date of the gradable item
+     */
+    public void addCourseGradable(Course course, String gradableItemName, String dueDate){
+        if(course.getGradableItems() == null){
+            course.setGradableItems(new HashMap<>());
+        }
+        course.getGradableItems().put(gradableItemName, dueDate);
+        db.collection("courses").document(course.getName()).set(course).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("SUCCESS", "GradableItem was successfully added");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("ERROR", "Could not add GradableItem", e);
+            }
+        });
     }
 }
